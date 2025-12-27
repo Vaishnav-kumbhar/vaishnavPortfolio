afe1c68dbaa4bf3afc82cbddc57cfe981cc448f4
Vaishnav-kumbhar <vk2222777@gmail.com>
2025-12-27 18:31:38 +0530

    remove scroll indicator from Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -128,12 +128,6 @@ export default function Hero() {
         </div>
       </div>
 
-      {/* Scroll Indicator */}
-      <div className="absolute bottom-4 left-1/2 -translate-x-1/2 animate-bounce hidden md:block">
-        <div className="w-6 h-10 border-2 border-white/30 rounded-full flex justify-center">
-          <div className="w-1 h-3 bg-white/50 rounded-full mt-2 animate-pulse"></div>
-        </div>
-      </div>
     </section>
   );
 }
